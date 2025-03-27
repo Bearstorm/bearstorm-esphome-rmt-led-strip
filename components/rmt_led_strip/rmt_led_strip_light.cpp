f26f37c5ea3720e7ebcfa67cdb660683844efa9f
Bearstorm <bearstormchannel@gmail.com>
2025-03-27 21:50:41 +0100

    Update rmt_led_strip_light.cpp

--- components/rmt_led_strip/rmt_led_strip_light.cpp
+++ components/rmt_led_strip/rmt_led_strip_light.cpp
@@ -10,14 +10,20 @@ static const char *const TAG = "rmt_led_strip";
 void RMTLedStripLight::setup() {
   ESP_LOGI(TAG, "Setting up RMT LED Strip on pin %d with %d LEDs", pin_->get_pin(), num_leds_);
 
-  // Inicializuj efektové vlastnosti
-  this->effects_.reserve(4);
-
-  this->addressable_traits_.set_max_power_color(255, 255, 255);
+  // Nastavenie základných vlastností svetla
   this->addressable_traits_.set_supports_rgb(true);
   this->addressable_traits_.set_supports_brightness(true);
   this->addressable_traits_.set_supports_effects(true);
+  this->addressable_traits_.set_max_power_color(255, 255, 255);
+
+  // Povinné pre správne rozpoznanie ESPHome
+  this->set_traits(this->addressable_traits_);
+
+  // Počet LED diód
   this->set_light_output_count(this->num_leds_);
+
+  // Efekty (možno neskôr rozšíriť)
+  this->effects_.reserve(4);
 }
 
 light::AddressableLightTraits RMTLedStripLight::get_traits() {
@@ -33,12 +39,12 @@ void RMTLedStripLight::write_state(light::LightState *state) {
   uint8_t g = green * 255 * brightness;
   uint8_t b = blue * 255 * brightness;
 
-  ESP_LOGD(TAG, "Writing RGB: R=%d G=%d B=%d", r, g, b);
+  ESP_LOGD(TAG, "Setting color: R=%d G=%d B=%d", r, g, b);
 
-  // Vypíš na všetky LEDky túto farbu (statický mód)
   for (uint16_t i = 0; i < this->size(); i++) {
     this->at(i) = Color(r, g, b);
   }
+
   this->schedule_show();
 }
 
