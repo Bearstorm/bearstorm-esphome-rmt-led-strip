4bf6faf3c2203586ca52f35bb8996aaeca529616
Bearstorm <bearstormchannel@gmail.com>
2025-03-27 21:29:18 +0100

    Update rmt_led_strip_light.cpp

--- components/rmt_led_strip/rmt_led_strip_light.cpp
+++ components/rmt_led_strip/rmt_led_strip_light.cpp
@@ -1,6 +1,6 @@
 #include "rmt_led_strip_light.h"
 #include "driver/rmt_tx.h"
-#include "esphome/core/log.h"
+#include "esp_log.h"
 
 namespace esphome {
 namespace rmt_led_strip {
@@ -8,26 +8,37 @@ namespace rmt_led_strip {
 static const char *const TAG = "rmt_led_strip";
 
 void RMTLedStripLight::setup() {
-  ESP_LOGI(TAG, "Initializing LED strip on pin %d with %d LEDs", pin_->get_pin(), num_leds_);
+  ESP_LOGI(TAG, "Setting up RMT LED Strip on pin %d with %d LEDs", pin_->get_pin(), num_leds_);
 
+  // Inicializuj efektové vlastnosti
   this->effects_.reserve(4);
 
   this->addressable_traits_.set_max_power_color(255, 255, 255);
   this->addressable_traits_.set_supports_rgb(true);
   this->addressable_traits_.set_supports_brightness(true);
   this->addressable_traits_.set_supports_effects(true);
-
   this->set_light_output_count(this->num_leds_);
 }
 
+light::AddressableLightTraits RMTLedStripLight::get_traits() {
+  return this->addressable_traits_;
+}
+
 void RMTLedStripLight::write_state(light::LightState *state) {
-  auto color = state->current_values.get_color();
+  float red, green, blue;
+  state->current_values_as_rgb(&red, &green, &blue);
   float brightness = state->current_values.get_brightness();
 
-  for (int i = 0; i < this->size(); i++) {
-    this->addressable_set(i, color.red * 255, color.green * 255, color.blue * 255);
-  }
+  uint8_t r = red * 255 * brightness;
+  uint8_t g = green * 255 * brightness;
+  uint8_t b = blue * 255 * brightness;
 
+  ESP_LOGD(TAG, "Writing RGB: R=%d G=%d B=%d", r, g, b);
+
+  // Vypíš na všetky LEDky túto farbu (statický mód)
+  for (uint16_t i = 0; i < this->size(); i++) {
+    this->at(i) = Color(r, g, b);
+  }
   this->schedule_show();
 }
 
