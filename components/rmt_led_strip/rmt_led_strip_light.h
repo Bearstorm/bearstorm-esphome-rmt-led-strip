f13f389d00e47b084abc75b2cbc31df1de7b7e2a
Bearstorm <bearstormchannel@gmail.com>
2025-03-27 20:26:57 +0100

    Update rmt_led_strip_light.h

--- components/rmt_led_strip/rmt_led_strip_light.h
+++ components/rmt_led_strip/rmt_led_strip_light.h
@@ -1,20 +1,20 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/light/addressable_light.h"
+#include "esphome/components/light/addressable_light.hpp"
+#include "esphome/core/gpio.h"
 
 namespace esphome {
 namespace rmt_led_strip {
 
-class RMTLedStripLight : public light::AddressableLight, public Component {
+class RMTLedStripLight : public light::AddressableLight {
  public:
   void setup() override;
-  void loop() override {}
 
   void set_pin(GPIOPin *pin) { pin_ = pin; }
   void set_num_leds(uint16_t num_leds) { num_leds_ = num_leds; }
-  void set_chipset(std::string chipset) { chipset_ = chipset; }
-  void set_rgb_order(std::string rgb_order) { rgb_order_ = rgb_order; }
+  void set_chipset(const std::string &chipset) { chipset_ = chipset; }
+  void set_rgb_order(const std::string &order) { rgb_order_ = order; }
   void set_rmt_channel(uint8_t channel) { rmt_channel_ = channel; }
 
  protected:
