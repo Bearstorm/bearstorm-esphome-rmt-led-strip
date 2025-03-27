6c299ba663cb8695d927c6f13ea76a1752a926fe
Bearstorm <bearstormchannel@gmail.com>
2025-03-27 21:22:51 +0100

    Update rmt_led_strip_light.cpp

--- components/rmt_led_strip/rmt_led_strip_light.cpp
+++ components/rmt_led_strip/rmt_led_strip_light.cpp
@@ -8,9 +8,10 @@ namespace rmt_led_strip {
 static const char *const TAG = "rmt_led_strip";
 
 void RMTLedStripLight::setup() {
-  ESP_LOGI(TAG, "Setting up RMT LED Strip on pin %d, %d LEDs", pin_->get_pin(), num_leds_);
+  ESP_LOGI(TAG, "Initializing LED strip on pin %d with %d LEDs", pin_->get_pin(), num_leds_);
 
   this->effects_.reserve(4);
+
   this->addressable_traits_.set_max_power_color(255, 255, 255);
   this->addressable_traits_.set_supports_rgb(true);
   this->addressable_traits_.set_supports_brightness(true);
@@ -23,15 +24,12 @@ void RMTLedStripLight::write_state(light::LightState *state) {
   auto color = state->current_values.get_color();
   float brightness = state->current_values.get_brightness();
 
-  ESP_LOGD(TAG, "Updating LEDs with color: R=%.2f G=%.2f B=%.2f Brightness=%.2f",
-           color.red, color.green, color.blue, brightness);
-
   for (int i = 0; i < this->size(); i++) {
     this->addressable_set(i, color.red * 255, color.green * 255, color.blue * 255);
   }
+
   this->schedule_show();
 }
 
 }  // namespace rmt_led_strip
 }  // namespace esphome
-
