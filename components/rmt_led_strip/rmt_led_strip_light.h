90a76fca2c860731e3e61cdee596c61e9a21460e
Bearstorm <bearstormchannel@gmail.com>
2025-03-27 21:02:43 +0100

    Update rmt_led_strip_light.h

--- components/rmt_led_strip/rmt_led_strip_light.h
+++ components/rmt_led_strip/rmt_led_strip_light.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/light/addressable_light.hpp"
 #include "esphome/core/gpio.h"
+#include "driver/rmt_tx.h"
 
 namespace esphome {
 namespace rmt_led_strip {
@@ -11,20 +12,21 @@ class RMTLedStripLight : public light::AddressableLight {
  public:
   void setup() override;
   void write_state(light::LightState *state) override;
-  float get_setup_priority() const override { return setup_priority::HARDWARE; }
 
   void set_pin(GPIOPin *pin) { pin_ = pin; }
   void set_num_leds(uint16_t num_leds) { num_leds_ = num_leds; }
   void set_chipset(const std::string &chipset) { chipset_ = chipset; }
   void set_rgb_order(const std::string &order) { rgb_order_ = order; }
-  void set_rmt_channel(uint8_t channel) { rmt_channel_ = channel; }
+  void set_rmt_channel(uint8_t channel) { rmt_channel_id_ = channel; }
 
  protected:
   GPIOPin *pin_;
   uint16_t num_leds_;
   std::string chipset_;
   std::string rgb_order_;
-  uint8_t rmt_channel_;
+  uint8_t rmt_channel_id_;
+
+  rmt_channel_handle_t rmt_channel_;
 };
 
 }  // namespace rmt_led_strip
