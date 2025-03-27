5f38c0b4513ac0489e5c7baec8f7b4a9135cb315
Bearstorm <bearstormchannel@gmail.com>
2025-03-27 21:18:42 +0100

    Update rmt_led_strip_light.cpp

--- components/rmt_led_strip/rmt_led_strip_light.cpp
+++ components/rmt_led_strip/rmt_led_strip_light.cpp
@@ -8,25 +8,30 @@ namespace rmt_led_strip {
 static const char *const TAG = "rmt_led_strip";
 
 void RMTLedStripLight::setup() {
-  ESP_LOGI(TAG, "Setting up RMT LED Strip on pin %d, %d LEDs", pin_->get_pin(), num_leds_);
+  ESP_LOGI(TAG, "Initializing LED strip on pin %d, %d LEDs", pin_->get_pin(), num_leds_);
 
   this->effects_.reserve(4);
   this->addressable_traits_.set_max_power_color(255, 255, 255);
   this->addressable_traits_.set_supports_rgb(true);
   this->addressable_traits_.set_supports_brightness(true);
   this->addressable_traits_.set_supports_effects(true);
-  this->set_light_output_count(this->num_leds_);
 
-  this->setup_light();
+  this->set_light_output_count(this->num_leds_);
 }
 
 void RMTLedStripLight::write_state(light::LightState *state) {
   auto color = state->current_values.get_color();
+  float brightness = state->current_values.get_brightness();
+
+  ESP_LOGD(TAG, "Setting color: R=%.2f G=%.2f B=%.2f (Brightness %.2f)",
+           color.red, color.green, color.blue, brightness);
+
   for (int i = 0; i < this->size(); i++) {
-    this->addressable_set(i, color.red, color.green, color.blue);
+    this->addressable_set(i, color.red * 255, color.green * 255, color.blue * 255);
   }
   this->schedule_show();
 }
 
 }  // namespace rmt_led_strip
 }  // namespace esphome
+
