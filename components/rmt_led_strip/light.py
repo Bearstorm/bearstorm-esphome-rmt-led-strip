51c11180b2aec8b8227e1a16bad8a0d386ef5dec
Bearstorm <bearstormchannel@gmail.com>
2025-03-27 20:17:03 +0100

    Update light.py

--- components/rmt_led_strip/light.py
+++ components/rmt_led_strip/light.py
@@ -3,6 +3,8 @@ import esphome.config_validation as cv
 from esphome.components import light
 from esphome.const import CONF_ID, CONF_PIN, CONF_NUM_LEDS, CONF_NAME
 
+from esphome import pins
+
 AUTO_LOAD = ['light']
 CODEOWNERS = [""]
 
@@ -11,7 +13,7 @@ RMTLedStripLight = rmt_led_strip_ns.class_('RMTLedStripLight', light.Addressable
 
 CONFIG_SCHEMA = light.ADDRESSABLE_LIGHT_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(RMTLedStripLight),
-    cv.Required(CONF_PIN): cv.pin,
+    cv.Required(CONF_PIN): pins.gpio_output_pin_schema,
     cv.Required(CONF_NUM_LEDS): cv.int_range(min=1),
     cv.Optional("chipset", default="ws2812"): cv.string_strict,
     cv.Optional("rgb_order", default="GRB"): cv.string_strict,
@@ -21,7 +23,10 @@ CONFIG_SCHEMA = light.ADDRESSABLE_LIGHT_SCHEMA.extend({
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield light.register_light(var, config)
-    cg.add(var.set_pin(config[CONF_PIN]))
+
+    pin = yield cg.gpio_pin_expression(config[CONF_PIN])
+    cg.add(var.set_pin(pin))
+
     cg.add(var.set_num_leds(config[CONF_NUM_LEDS]))
     cg.add(var.set_chipset(config["chipset"]))
     cg.add(var.set_rgb_order(config["rgb_order"]))
