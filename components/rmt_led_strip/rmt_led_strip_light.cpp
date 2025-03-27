54259f3a89176f0d43962b6c22eefbc0c9e2e3fa
Bearstorm <bearstormchannel@gmail.com>
2025-03-27 20:50:53 +0100

    Update rmt_led_strip_light.cpp

--- components/rmt_led_strip/rmt_led_strip_light.cpp
+++ components/rmt_led_strip/rmt_led_strip_light.cpp
@@ -1,5 +1,4 @@
 #include "rmt_led_strip_light.h"
-#include "driver/rmt_tx.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
@@ -8,17 +7,28 @@ namespace rmt_led_strip {
 static const char *const TAG = "rmt_led_strip";
 
 void RMTLedStripLight::setup() {
-  ESP_LOGI(TAG, "Initializing LED strip on pin %d with %d LEDs", pin_->get_pin(), num_leds_);
+  ESP_LOGI(TAG, "Initializing RMT LED Strip on pin %d with %d LEDs", pin_->get_pin(), num_leds_);
 
-  // Inicializuj adresovateľný LED pás
   this->effects_.reserve(4);
   this->addressable_traits_.set_max_power_color(255, 255, 255);
   this->addressable_traits_.set_supports_rgb(true);
   this->addressable_traits_.set_supports_brightness(true);
   this->addressable_traits_.set_supports_effects(true);
   this->set_light_output_count(this->num_leds_);
+}
 
-  // TODO: Pridať RMT ovládanie (zatiaľ len základ)
+void RMTLedStripLight::write_state(light::LightState *state) {
+  auto call = state->current_values;
+  float r = call.get_red();
+  float g = call.get_green();
+  float b = call.get_blue();
+  float brightness = call.get_brightness();
+
+  ESP_LOGD(TAG, "Setting LED color R:%.2f G:%.2f B:%.2f Brightness: %.2f", r, g, b, brightness);
+
+  // Tu môžeš neskôr pridať kód pre posielanie dát do pásika cez RMT
+
+  // Zatiaľ žiadna RMT logika – len debug info
 }
 
 }  // namespace rmt_led_strip
