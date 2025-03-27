52341b4251ba9124abcb047816ab96ccfe1aae41
Bearstorm <bearstormchannel@gmail.com>
2025-03-27 20:34:06 +0100

    Update rmt_led_strip_light.cpp

--- components/rmt_led_strip/rmt_led_strip_light.cpp
+++ components/rmt_led_strip/rmt_led_strip_light.cpp
@@ -1,21 +1,24 @@
 #include "rmt_led_strip_light.h"
-#include "esphome/components/light/addressable_light.hpp"
 #include "driver/rmt_tx.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
 namespace rmt_led_strip {
 
-// Príklad setup funkcie
-void RMTLedStripLight::setup() {
-  ESP_LOGI("rmt_led_strip", "Initializing LED strip on pin %d", pin_->get_pin());
+static const char *const TAG = "rmt_led_strip";
 
+void RMTLedStripLight::setup() {
+  ESP_LOGI(TAG, "Initializing LED strip on pin %d with %d LEDs", pin_->get_pin(), num_leds_);
+
+  // Inicializuj adresovateľný LED pás
   this->effects_.reserve(4);
   this->addressable_traits_.set_max_power_color(255, 255, 255);
   this->addressable_traits_.set_supports_rgb(true);
   this->addressable_traits_.set_supports_brightness(true);
   this->addressable_traits_.set_supports_effects(true);
   this->set_light_output_count(this->num_leds_);
+
+  // TODO: Pridať RMT ovládanie (zatiaľ len základ)
 }
 
 }  // namespace rmt_led_strip
